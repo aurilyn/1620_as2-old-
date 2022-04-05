10b84e51ec4ebd1625a3f496391ee0d914df0bb7
BennyCao <caobenny122@gmail.com>
2022-04-05 14:56:59 -0700

    added textarea when button is clicked

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+saveTextBtn = document.querySelector('.icons')
+
 const notes = [
   { 
     title: "first note", 
@@ -5,3 +7,28 @@ const notes = [
     id: 1 
   }
 ]
+function newNote(noteBody){
+  const template = `
+    <p>${noteBody}<p>
+  `
+  return template
+}
+
+function displayNote(note) {
+  const noteDisplayArea = document.querySelector('.create-note-area')
+  noteDisplayArea.innerHTML = ''
+  noteDisplayArea.insertAdjacentHTML('afterbegin', note)
+}
+
+function getNote (){
+  const textArea = document.querySelector('.fa-solid fa-circle-plus')
+  const note = textArea.value
+  return note
+}
+
+function assembeNote (){
+  const noteText = getNote()
+  const note = newNote(noteText)
+  displayNote(note)
+}
+saveTextBtn.addEventListener('click', assembeNote)
